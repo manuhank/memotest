224fe44d961abde17f19d26d7db9ca7526d0652c
Manuel Puentes <manuhank@gmail.com>
2022-12-13 17:09:49 -0300

    improved Seed

--- memotest/database/seeders/DatabaseSeeder.php
+++ memotest/database/seeders/DatabaseSeeder.php
@@ -18,8 +18,22 @@ class DatabaseSeeder extends Seeder
         // \App\Models\Memotest::factory(2)->create();
 
         DB::table('memotests')->insert([
-            'name' => 'Test',
-            'urls' => json_encode(array("https://www.cippec.org/wp-content/uploads/2022/11/whatsapp-image-2022-11-23-at-12018-pm-1024x576.jpeg", "https://ichef.bbci.co.uk/news/640/cpsprodpb/66C6/production/_120501362_1.jpg")),
+            'name' => 'Capitals of Europe',
+            'urls' => json_encode([
+                "https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/large_jpg/eiffel-tower-in-paris-151-medium.jpg?1564742900",
+                "https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTYyNDg1MjE3MTI1Mjc5Mzk4/topic-london-gettyimages-760251843-promo.jpg",
+                "https://www.germany.travel/media/redaktion/content/bundeslaender/berlin/Berlin_Brandenburger_Tor_im_Sonnenuntergang_Leitmotiv_German_Summer_Cities.jpg",
+                "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/34/00/95/community-of-madrid.jpg?w=700&h=500&s=1"
+            ]),
+        ]);
+        DB::table('memotests')->insert([
+            'name' => 'Landscapes',
+            'urls' => json_encode([
+                "https://images.unsplash.com/photo-1610878180933-123728745d22?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FuYWRhJTIwbmF0dXJlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+                "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-landscape-1648265299.jpg?crop=0.676xw:1.00xh;0.148xw,0&resize=640:*",
+                "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.undp.org%2Fchina%2Fblog%2Fwhy-humanity-must-save-nature-save-itself&psig=AOvVaw2dwJ_rgYQrkJ_zMEBM0xWv&ust=1671048473146000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKDI7uay9_sCFQAAAAAdAAAAABAf",
+                "https://www.youandthemat.com/wp-content/uploads/nature-2-26-17.jpg"
+            ]),
         ]);
     }
 }
